034fb74adef6d4de0154fe0a43d74d1d8952e5b9
Niccolò Biondi <niccolo.biondi1@stud.unifi.it>
2020-03-18 21:21:00 +0100

    bash script for test on len seq with cuda (8values) and nthreads in query par (TODO lenseq with 8 on OMP)

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
 # running tests on len Seq on CUDA with 8 testing values (not 4)
 
 ./CUDAlenSeq 100000 1000 50 10 8 n 0
-./CUDAlenSeq 100000 1000 50 10 8 n 0
-./CUDAlenSeq 100000 1000 50 10 8 n 0
-./CUDAlenSeq 100000 1000 50 10 8 n 0
+./CUDAlenSeq 100000 1000 50 10 8 p 0
+./CUDAlenSeq 100000 1000 50 10 8 t 0
+./CUDAlenSeq 100000 1000 50 10 8 c 0
 
 # running test on threads number with query paral in OMP
 
